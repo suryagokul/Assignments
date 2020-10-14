0d8a60d2984d483d1ce8e9e2b413734e258e87d2
suryagokul <41636357+suryagokul@users.noreply.github.com>
2020-10-14 18:11:23 +0530

    Add files via upload

---
+++ Ass 1b.py
@@ -0,0 +1,3 @@
+FN, LN = input("Enter First Name and Last Name separated with space >> ").split()
+
+print(FN[::-1], LN[::-1], sep=" ")
---
+++ Ass 1c.py
@@ -0,0 +1,5 @@
+diameter = 12
+
+radius = diameter/2
+
+print((4/3)*3.1415*radius**3)
